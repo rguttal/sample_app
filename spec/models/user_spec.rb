b24af0067f95c7152a7cb4553961759b096f3e55
rguttal <guttal.rahul@gmail.com>
2014-01-16 16:36:49 -0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,10 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
 
   it { should be_valid }
+
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -108,6 +110,9 @@ describe User do
     end
   end
 
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 end
